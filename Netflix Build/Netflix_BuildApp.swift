89f25de0c44d93d7accb909a449b187fbe744bd7
Stanley Pan <imoment.developer@gmail.com>
2021-12-22 14:52:04 -0500

    Initial Commit

---
+++ Netflix Build/Netflix_BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_BuildApp.swift
+//  Netflix Build
+//
+//  Created by Stanley Pan on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
