4ab066188a861868946452122a5d4027807eb07c
Stanley Pan <imoment.developer@gmail.com>
2021-12-27 16:31:13 -0500

    Test commit for added account

--- Netflix Build/Search/SearchBar.swift
+++ Netflix Build/Search/SearchBar.swift
@@ -29,7 +29,7 @@ struct SearchBar: View {
                     .foregroundColor(.white)
                 
                 Button(action: {
-                    // TODO: clear text
+                    // TODO: clear text, test commit
                 }, label: {
                     Image(systemName: "xmark.circle.fill")
                         .foregroundColor(.graySearchText)
