eae0b84fbec29b996371814721e2fb0c3fe8cb5c
Stanley Pan <imoment.developer@gmail.com>
2021-12-28 16:36:19 -0500

    Added SearchResultsGrid, placed into SearchView, and implemented SearchView into ContentView

--- Netflix Build/ContentView.swift
+++ Netflix Build/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView: View {
                     Text("Home")
                 }.tag(0)
             
-            Text("SearchView")
+            SearchView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                     Text("Search")
