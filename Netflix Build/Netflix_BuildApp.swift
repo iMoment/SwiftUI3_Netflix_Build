b94ff2992f59bcba67da58af6e2e9fbc77dc2db3
Stanley Pan <imoment.developer@gmail.com>
2021-12-29 16:15:40 -0500

    Added PagerView

--- Netflix Build/Netflix_BuildApp.swift
+++ Netflix Build/Netflix_BuildApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Netflix_BuildApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            PagerDummy()
         }
     }
 }
